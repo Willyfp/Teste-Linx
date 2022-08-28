9c12025670e61356476603e312faf8cdbe620239
willy <willypereira41@gmail.com>
2022-08-28 15:58:34 -0300

    Paginação e melhorias nos botões

--- src/scripts/listProducts.js
+++ src/scripts/listProducts.js
@@ -1,11 +1,34 @@
 const listProducts = document.querySelector(".list-products");
 
-axios
-  .get(
-    "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1"
-  )
-  .then((res) => {
+var nextPage =
+  "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1";
+
+const createProduct = (product) => {
+  const { image, name, oldPrice, price, description, installments } = product;
+
+  return `
+      <div class="product">
+        <div class="image-div">
+          <img src="https:${image}" alt="${name}" />
+        </div>
+  
+        <div class="info-product">
+          <h3>${name}</h3>
+          <p class="description">${description}</p>
+          <p>De: R$${oldPrice.toFixed(2)}</p>
+          <h2>Por: R$${price.toFixed(2)}</h2>
+          <p>ou ${installments.count}x de R$${installments.value.toFixed(2)}</p>
+          <button class="button button-product">Comprar</button>
+        </div>
+      </div>
+    `;
+};
+
+const renderNextPage = () => {
+  axios.get(nextPage).then((res) => {
     const { data } = res;
+
+    nextPage = `https://${data.nextPage}`;
 
     data.products.forEach((product) => {
       const newProduct = createProduct(product);
@@ -13,24 +36,6 @@ axios
       listProducts.innerHTML += newProduct;
     });
   });
-
-const createProduct = (product) => {
-  const { image, name, oldPrice, price, description, installments } = product;
-
-  return `
-    <div class="product">
-      <div class="image-div">
-        <img src="https:${image}" alt="${name}" />
-      </div>
-
-      <div class="info-product">
-        <h3>${name}</h3>
-        <p class="description">${description}</p>
-        <p>De: R$${oldPrice.toFixed(2)}</p>
-        <h2>Por: R$${price.toFixed(2)}</h2>
-        <p>ou ${installments.count}x de R$${installments.value.toFixed(2)}</p>
-        <button class="button button-product">Comprar</button>
-      </div>
-    </div>
-  `;
 };
+
+renderNextPage();
