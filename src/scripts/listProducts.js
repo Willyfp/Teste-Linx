212cf56a21601662a1eb248ad62d14b45698dea4
willy <willypereira41@gmail.com>
2022-08-28 15:24:30 -0300

    Estilização e responsividade dos produtos

--- src/scripts/listProducts.js
+++ src/scripts/listProducts.js
@@ -17,17 +17,20 @@ axios
 const createProduct = (product) => {
   const { image, name, oldPrice, price, description, installments } = product;
 
-  console.log(image);
-
   return `
     <div class="product">
-      <img src="https:${image}" alt="${name}" />
-      <h3>${name}</h3>
-      <p>${description}</p>
-      <p>De: R$${oldPrice.toFixed(2)}</p>
-      <h2>Por: R$${price.toFixed(2)}</h2>
-      <p>ou ${installments.count}x de R$${installments.value.toFixed(2)}</p>
-      <button class="button button-product">Comprar</button>
+      <div class="image-div">
+        <img src="https:${image}" alt="${name}" />
+      </div>
+
+      <div class="info-product">
+        <h3>${name}</h3>
+        <p class="description">${description}</p>
+        <p>De: R$${oldPrice.toFixed(2)}</p>
+        <h2>Por: R$${price.toFixed(2)}</h2>
+        <p>ou ${installments.count}x de R$${installments.value.toFixed(2)}</p>
+        <button class="button button-product">Comprar</button>
+      </div>
     </div>
   `;
 };
